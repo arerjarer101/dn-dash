970725ec2091df3a41ab79293418b050fe26abf8
arerjarer101 <arerjarer@gmail.com>
2023-06-05 15:52:58 +0300

    working on vueprime

--- dn-dash/src/main.js
+++ dn-dash/src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import './assets/app.css';
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(ToastService);
+
+app.component('Button', Button);
+app.component('InputText', InputText); 
+app.component('Toast', Toast); 
+
+app.mount("#app");
---
+++ dn-dash/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import LoadingPage from './LoginPage.vue'
+
+</script>
+
+<template>
+  <div class="container">
+    <LoadingPage></LoadingPage>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
